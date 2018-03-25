5e0cc76bc15afe17fe82cc21dc0d98946e37285d
Daniel Hartnell <danielhartnell@gmail.com>
2018-03-24 19:19:22 -0700

    not sure what I did

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,5 +7,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Social Fun Times';
-  events = ['event1', 'event2', 'event3', 'event4']
+  events = [
+  	{
+  		'name': 'Cascade brewing',
+  		'location': '939 SE Belmont St, Portland, OR 97214',
+  		'votes': 0
+  	},
+  	{
+  		'name': 'McMenamins Blue Moon Tavern & Grill',
+  		'location': '432 NW 21st Ave, Portland, OR 97209',
+  		'votes': 0
+  	},
+  	{
+  		'name': 'Ex Novo Brewing Co.',
+  		'location': '2326 N Flint Ave, Portland, OR 97227',
+  		'votes': 0
+  	}
+  ]
 }
